b9536e1173bf56efa8158c305a33cd7338391613
kimhanwoong <khw7096@gmail.com>
2016-06-19 21:36:55 +0900

    add instance num

--- python/scan2grey.py
+++ python/scan2grey.py
@@ -1,7 +1,8 @@
-
+#coding:utf8
 import NatronEngine
 import NatronGui
 
 def scan2grey():
-	app = NatronGui.natron.getGuiInstance(0)
+	inum = NatronEngine.natron.getNumInstances() #최초 1을 반환한다.
+	app = NatronGui.natron.getGuiInstance(inum-1)
 	t = app.createNode("fr.inria.openfx.ReadOIIO")
