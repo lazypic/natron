87ecd3ee6f81d9128bd94cc2c1782160aea82a16
kimhanwoong <khw7096@gmail.com>
2016-06-19 22:02:12 +0900

    add node

--- initGui.py
+++ initGui.py
@@ -2,5 +2,4 @@
 
 import NatronEngine
 NatronEngine.natron.appendToNatronPath("~/natronset/python")
-import scan2grey
-NatronGui.natron.addMenuCommand("lazypic/Scripts/Scan2grey","scan2grey.scan2grey")
+NatronGui.natron.addMenuCommand("lazypic/Scripts/Scan2alpha","test")
