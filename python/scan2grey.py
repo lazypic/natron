01675d3125f7e24e1443ecae213dd9dc87bd3c51
kimhanwoong <khw7096@gmail.com>
2016-06-19 22:01:52 +0900

    add scan2alpha

--- python/scan2grey.py
+++
@@ -1,8 +0,0 @@
-#coding:utf8
-import NatronEngine
-import NatronGui
-
-def scan2grey():
-	inum = NatronEngine.natron.getNumInstances() #최초 1을 반환한다.
-	app = NatronGui.natron.getGuiInstance(inum-1)
-	t = app.createNode("fr.inria.openfx.ReadOIIO")
